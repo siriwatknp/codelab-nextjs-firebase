1e43656e00a1a17e490a8775f1ccaa8003ca847e
siriwatknp <siriwatkunaporn@gmail.com>
2024-07-07 15:46:34 +0700

    Listen for realtime restaurant updates

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -42,15 +42,61 @@ export async function addReviewToRestaurant(db, restaurantId, review) {
 }
 
 function applyQueryFilters(q, { category, city, price, sort }) {
-	return;
+	if (category) {
+					q = query(q, where("category", "==", category));
+	}
+	if (city) {
+					q = query(q, where("city", "==", city));
+	}
+	if (price) {
+					q = query(q, where("price", "==", price.length));
+	}
+	if (sort === "Rating" || !sort) {
+					q = query(q, orderBy("avgRating", "desc"));
+	} else if (sort === "Review") {
+					q = query(q, orderBy("numRatings", "desc"));
+	}
+	return q;
 }
 
 export async function getRestaurants(db = db, filters = {}) {
-	return [];
+	let q = query(collection(db, "restaurants"));
+
+	q = applyQueryFilters(q, filters);
+	const results = await getDocs(q);
+	return results.docs.map(doc => {
+					return {
+									id: doc.id,
+									...doc.data(),
+									// Only plain objects can be passed to Client Components from Server Components
+									timestamp: doc.data().timestamp.toDate(),
+					};
+	});
 }
 
 export function getRestaurantsSnapshot(cb, filters = {}) {
-	return;
+	if (typeof cb !== "function") {
+					console.log("Error: The callback parameter is not a function");
+					return;
+	}
+
+	let q = query(collection(db, "restaurants"));
+	q = applyQueryFilters(q, filters);
+
+	const unsubscribe = onSnapshot(q, querySnapshot => {
+					const results = querySnapshot.docs.map(doc => {
+									return {
+													id: doc.id,
+													...doc.data(),
+													// Only plain objects can be passed to Client Components from Server Components
+													timestamp: doc.data().timestamp.toDate(),
+									};
+					});
+
+					cb(results);
+	});
+
+	return unsubscribe;
 }
 
 export async function getRestaurantById(db, restaurantId) {
